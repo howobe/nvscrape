ed6efd71665b48249d3fb9c8f026becedb05c957
howobe <howobe@howobe.howobe>
2020-11-07 21:16:23 +0000

    First commit

---
+++ notify.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import abc
+import smtplib
+import ssl
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from slack import WebClient
+from slack.errors import SlackApiError
+import logging
+
+slackLogger = logging.getLogger("slack")
+emailLogger = logging.getLogger("email")
+
+logging.basicConfig(level=logging.INFOs)
+
+class NotifierInterface(metaclass=abc.ABCMeta):
+
+    @classmethod
+    def __subclasshook__(cls, subclass):
+        return (hasattr(subclass, 'send') and
+                callable(subclass.send) or
+                NotImplemented)
+
+    @abc.abstractmethod
+    def send(self):
+        raise NotImplementedError
+
+
+class EmailNotification(NotifierInterface):
+
+    def __init__(self, emailAddress: str, emailPsk: str, port: int = 465):
+        self.user = {"email": emailAddress, "psk": emailPsk}
+        self.port = port
+
+    def addRecipients(self, *args):
+        if not hasattr(self, "recipients"):
+            self.recipients = []
+        self.recipients.extend(args)
+
+    def setSubject(self, subject: str):
+        self.subject = subject
+
+    def setBody(self, body: str):
+        self.body = body
+
+    def send(self):
+        message = MIMEMultipart()
+        message["From"] = self.user["email"]
+        message["To"] = ", ".join(self.recipients)
+        message["Subject"] = self.subject
+        message.attach(MIMEText(self.body, "plain"))
+        msg = message.as_string()
+
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL("smtplib.gmail.com", self.port, context=context)\
+                as server:
+            server.login(self.user["email"], self.user["psk"])
+            server.sendmail(self.user["email"], self.recipients, msg)
+
+
+class SlackNotification(NotifierInterface):
+
+    def __init__(self, apiToken: str):
+        self.client = WebClient(apiToken)
+        slackLogger.info("Initiating Slack WebClient...")
+
+    def setBody(self, message):
+        self.message = message
+        slackLogger.debug("Message set: " + self.message)
+
+    def send(self):
+        try:
+            self.client.chat_postMessage(
+                channel='#notifications',
+                text=self.message)
+        except SlackApiError as e:
+            assert e.response["ok"] is False
+            assert e.response["error"]
+            slackLogger.exception(e)
---
+++ nvscrape.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import requests_html
+from bs4 import BeautifulSoup
+from bs4.element import Tag as bs4Tag
+import json
+import logging
+from logging import config as logconfig
+import yaml
+from notify import SlackNotification
+import os
+
+
+with open('nvlogger.yaml', 'r') as f:
+    config = yaml.safe_load(f.read())
+    logconfig.dictConfig(config)
+
+nvlog = logging.getLogger("nv")
+mainlog = logging.getLogger("main")
+
+
+def htmlRequest(url: str) -> requests_html.HTMLResponse:
+    nvlog.debug("Starting session...")
+    session = requests_html.HTMLSession()
+    nvlog.info(f"Making GET request: url={url}")
+    response = session.get(url)
+    nvlog.info(f"Response received ({response.status_code})")
+    nvlog.debug("Rendering dynamic content...")
+    response.html.render()
+    nvlog.info("Rendered dynamic content")
+    nvlog.debug("Closing session...")
+    session.close()
+    return response.html.raw_html
+
+
+def parse(responseStr: str, parser: str = 'lxml') -> BeautifulSoup:
+    nvlog.info(f"Parsing response: parser: {parser}")
+    return BeautifulSoup(responseStr, parser)
+
+
+def getDiv(parsedResponse: BeautifulSoup, className: str = None):
+    nvlog.debug(f"getDiv called: className={className}")
+    if className is not None:
+        div = parsedResponse.find("div", {"class": className})
+    else:
+        div = parsedResponse.find("div")
+    nvlog.debug(f"div: {div}")
+    return div
+
+
+def getDivs(parsedResponse: BeautifulSoup, className: str = None) -> list:
+    nvlog.debug(f"getDivs called: className={className}")
+    if className is not None:
+        divs = parsedResponse.find_all("div", {"class": className})
+    else:
+        divs = parsedResponse.find_all("div")
+    nvlog.debug(f"divs: {divs}")
+    return divs
+
+
+def itemJson(div: bs4Tag):
+    try:
+        json.loads(div.text)
+    except json.JSONDecodeError as e:
+        nvlog.exception(e)
+
+
+def getValues(dictionary: dict, *args) -> list:
+    res = []
+    for k in args:
+        try:
+            res.append(dictionary[k])
+        except KeyError as e:
+            nvlog.warning(f"key '{k}' not in {dictionary.keys()}")
+            nvlog.exception(e)
+            continue
+    return res
+
+
+r = htmlRequest("https://www.nvidia.com/en-gb/shop/geforce/gpu/?page=1" +
+                "&limit=9&locale=en-gb&category=GPU&gpu=RTX%203070")
+soup = parse(r)
+rtx3070info = itemJson(getDiv(soup, "NVGFT070"))
+if rtx3070info is not None:
+    mainlog.info("Field no longer empty")
+    if not isinstance(rtx3070info, list):
+        rtx3070info = [rtx3070info]
+    links = []
+    for dic in rtx3070info:
+        links.extend(getValues(dic, "directPurchaseLink", "purchaseLink"))
+    linksStr = ", ".join(links)
+    sl = SlackNotification(os.environ["SLACK_API_TOKEN"])
+    sl.setBody(f"Change detected!\nTry these links: {linksStr}\nBonus: " +
+               "https://secure.scan.co.uk/web/basket/addproduct/3236417")
+    sl.send()
